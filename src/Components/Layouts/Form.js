9840a06f0cd1e82decc60a91e54cd516324ab0df
Corey Hawkins <corey@Coreys-Air.lan>
2019-08-29 10:05:21 -0700

    Button Collects Form Data

--- src/Components/Layouts/Form.js
+++ src/Components/Layouts/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { FormControl, Input, InputLabel, makeStyles  } from '@material-ui/core';
 import Exercises from '../Exercises/index'
 
@@ -21,50 +21,68 @@ inner: {
   marginLeft: 'auto',
   marginRight: 'auto',
 },
-
+button: {
+  display: 'flex',
+  position: 'relative',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+}
 }))
+let idArray = ['title-input', 'company-input', 'url-input', 'contact-input', 'email-input', 'linkedIn-input', 'phone-input']
+let inputArray = []
+
 
 export default function InputForm() {
     const classes = useStyles();
+    const [useDataCollect] = useState([
+      {
+        clickFunc: () => {
+          for (let i=0; i<idArray.length; i++){
+            inputArray[i] = document.getElementById(idArray[i]).value
+          }
+          return console.log(inputArray)
+      }
+    }
+    ])
 
   return (
     <div className={classes.outer}>
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="title--input" className={classes.inner}>Job Title</InputLabel>
-    <Input id="title-input" />
+    <InputLabel  className={classes.inner}>Job Title</InputLabel>
+    <Input id="title-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="company-input" className={classes.inner}>Company Name</InputLabel>
-    <Input id="company-input" />
+    <InputLabel  className={classes.inner}>Company Name</InputLabel>
+    <Input id="company-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="url-input" className={classes.inner}>Url Address</InputLabel>
-    <Input id="url-input" />
+    <InputLabel  className={classes.inner}>Url Address</InputLabel>
+    <Input id="url-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="contact-input" className={classes.inner}>Contact Name</InputLabel>
-    <Input id="contact-input" />
+    <InputLabel className={classes.inner}>Contact Name</InputLabel>
+    <Input id="contact-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="email-input" className={classes.inner}>Email address</InputLabel>
-    <Input id="email-input" />
+    <InputLabel className={classes.inner}>Email address</InputLabel>
+    <Input id="email-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="linkedIn-input" className={classes.inner}>LinkedIn Name</InputLabel>
-    <Input id="linkedIn-input" />
+    <InputLabel  className={classes.inner}>LinkedIn Name</InputLabel>
+    <Input id="linkedIn-input" className={classes.inner}/>
   </FormControl>
 
   <FormControl className={classes.root}>
-    <InputLabel htmlFor="phone-input" className={classes.inner}>Phone #</InputLabel>
-    <Input id="phone-input" />
+    <InputLabel  className={classes.inner}>Phone #</InputLabel>
+    <Input id="phone-input" className={classes.inner}/>
   </FormControl>
 
-  // <button onClick={this.state.clickFunc}> Click Me?</button>
+<button className="classes.button" onClick={useDataCollect[0].clickFunc}> Click Me?</button>
 </div>
   )
 };
